2003616bbf50452ab209ce01889e5025b0661be8
Chikara Miki <chikara.miki0905@icloud.com>
2023-01-28 15:49:22 +0000

    add: hover feature to the toggle

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
   root 'pages#home'
   
-  get '/project' => 'pages#project'
+  get 'project' => 'pages#project'
 
-  get '/team' => 'pages#team'
-  get '/aerostrucutre' => 'teams#aerostrucutre'
-  get '/avionics' => 'teams#avionics'
-  get '/other_team' => 'teams#other_team'
+  get 'team' => 'pages#team'
+  get '/team/aerostrucutre' => 'team#aerostrucutre'
+  get '/team/avionics' => 'team#avionics'
+  get '/team/other_team' => 'team#other_team'
 
-  get '/idea' => 'pages#idea'
-  get '/achievements' => 'pages#achievements'
-  get '/recruitment' => 'pages#recruitment'
-  get '/contact' => 'pages#contact'
+  get 'idea' => 'pages#idea'
+  get 'achievements' => 'pages#achievements'
+  get 'recruitment' => 'pages#recruitment'
+  get 'contact' => 'pages#contact'
 end
