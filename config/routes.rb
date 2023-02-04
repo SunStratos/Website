7490020b3b23316468ea8690b464f6d53230a1f0
Chikara Miki <103758881+SMALLASIAN@users.noreply.github.com>
2023-02-04 21:31:25 +0900

    Navbar (#8)
    * add: initial nav bar
    
    * fix: logo size
    
    * fix paddings on navbar
    
    * add: each pages view
    
    * add: path on the routes.rb/syntax errors needs to be fixed
    
    * fix: routes.rb
    
    * add: bootswatch gemfile
    
    * remove: bootswatch from the application.erb
    
    * fix: toggle bar
    
    * add: hover feature to the toggle
    
    * comment out the react for now
    
    * add: another href within the team page
    
    * fix: controllers
    
    * add: links to the team page
    
    * created js and scss for navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
-  root 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'home#index'
+
+  get '/achievement' => 'achievement#index'
+  get '/recruitment' => 'recruitment#index'
+  get '/contact' => 'contact#index'
+  get '/idea' => 'idea#index'
+  get '/project' =>'project#index'
+
+  get '/team' => 'team#index'
+  get '/team/aerostructure' => 'team#aerostructure'
+  get '/team/avionics' => 'team#avionics'
+  get '/team/C' => 'team#C'
+  get '/team/D' => 'team#D'
+  get '/team/E' => 'team#E'
 end
