a699ec1b27c7cc5fb8a872d03082018a4ce878bc
Chikara Miki <chikara.miki0905@icloud.com>
2023-02-09 20:31:27 +0000

    add: yml folders and set up the locale:

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 Rails.application.routes.draw do
   root 'home#index'
 
-  get '/achievement' => 'achievement#index'
-  get '/recruitment' => 'recruitment#index'
-  get '/contact' => 'contact#index'
-  get '/idea' => 'idea#index'
-  get '/project' =>'project#index'
+  get '/achievement' => 'achievement#index', :as => :achievement
+  get '/recruitment' => 'recruitment#index', :as => :recruitment
+  get '/contact' => 'contact#index', :as => :contact
+  get '/idea' => 'idea#index', :as => :idea
+  get '/project' =>'project#index', :as => :project
 
   get '/team' => 'team#index'
-  get '/team/aerostructure' => 'team#aerostructure'
-  get '/team/avionics' => 'team#avionics'
-  get '/team/C' => 'team#C'
-  get '/team/D' => 'team#D'
-  get '/team/E' => 'team#E'
+  get '/team/aerostructure' => 'team#aerostructure', :as => :team_aerostructure
+  get '/team/avionics' => 'team#avionics', :as => :team_avionics
+  get '/team/C' => 'team#C', :as => :team_C
+  get '/team/D' => 'team#D', :as => :team_D
+  get '/team/E' => 'team#E', :as => :team_E
 end
